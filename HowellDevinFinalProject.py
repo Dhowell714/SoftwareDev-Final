105d1ea271a7eb0904f384a4be5684eecf9235ac
Luna Silver <drhowell714@gmail.com>
2022-03-07 11:23:25 -0500

    2

--- HowellDevinFinalProject.py
+++ HowellDevinFinalProject.py
@@ -1,5 +1,27 @@
-import tkinter as tk
-window = tk.Tk()
-window.title('Pet Quest')
+from tkinter import *
 
-window.mainloop()
+app = Tk()
+
+# Pet Supplies------------------------------------
+pet_text = StringVar()
+pet_label = Label(app, text='Pet Supply Name', font=('bold', 14))
+pet_label.grid(row=0, column=2, sticky=W)
+pet_entry = Entry(app, textvariable=pet_text)
+pet_entry.grid(row=0, column=1)
+# Customer --------------------------------------
+customer_text = StringVar()
+customer_label = Label(app, text='Customer Name', font=('bold', 14), pady=20)
+customer_label.grid(row=0, column=0, sticky=W)
+customer_entry = Entry(app, textvariable=customer_text)
+customer_entry.grid(row=0, column=3)
+# Retailer ------------------------------------------
+retailer_text = StringVar()
+retailer_label = Label(app, text='Retailer Name', font=('bold', 14), pady=20)
+retailer_label.grid(row=1, column=0, sticky=W)
+retailer_entry = Entry(app, textvariable=retailer_text)
+retailer_entry.grid(row=1, column=1)
+
+app.title('Pet Quest')
+app.geometry('700x350')
+
+app.mainloop()
