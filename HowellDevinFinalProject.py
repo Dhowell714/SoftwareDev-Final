0b2b1da49dca5a5f6df9a768b229f777b619db51
Luna Silver <drhowell714@gmail.com>
2022-03-08 19:24:42 -0500

    Evening Update

--- HowellDevinFinalProject.py
+++ HowellDevinFinalProject.py
@@ -1,5 +1,20 @@
 from tkinter import *
 
+def populate_list():
+    print('Populate')
+
+def add_item():
+    print('Add')
+
+def remove_item():
+    print('Remove')
+
+def update_item():
+    print('Update')
+
+def clear_text():
+    print('Clear')
+
 app = Tk()
 
 # Pet Supplies------------------------------------
@@ -20,8 +35,36 @@ retailer_label = Label(app, text='Retailer Name', font=('bold', 14), pady=20)
 retailer_label.grid(row=1, column=0, sticky=W)
 retailer_entry = Entry(app, textvariable=retailer_text)
 retailer_entry.grid(row=1, column=1)
+# Price -----------------------------------------------
+price_text = StringVar()
+price_label = Label(app, text='Price', font=('bold', 14), pady=20)
+price_label.grid(row=1, column=2, sticky=W)
+price_entry = Entry(app, textvariable=price_text)
+price_entry.grid(row=1, column=3)
+# List of Supplies -------------------------------------
+pet_list = Listbox(app, height=8, width=50, border=0)
+pet_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
+
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+pet_list.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=pet_list.yview)
+
+add_btn = Button(app, text='Add Pet Supplies', width=12, command=add_item)
+add_btn.grid(row=2, column=0, pady=20)
+
+remove_btn = Button(app, text='Remove Pet Supplies', width=12, command=remove_item)
+remove_btn.grid(row=2, column=1)
+
+update_btn = Button(app, text='Update Pet Supplies', width=12, command=update_item)
+update_btn.grid(row=2, column=2)
+
+clear_btn = Button(app, text='Clear Input', width=12, command=clear_text)
+clear_btn.grid(row=2, column=3)
 
 app.title('Pet Quest')
 app.geometry('700x350')
+
+populate_list()
 
 app.mainloop()
---
+++ db.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db):
+       self.conn = sqlite3.connect(db)
+       self.cur = self.conn.cursor()
+       self.cur.execute("CREATE TABLE IF NOT EXISTS supplies (id INTEGER PRIMARY KEY, pet text, customer text, retailer text, price text")
+       self.conn. commit()
+
+    def fetch(self):
+        self.cur.execute("SELECT * FROM pet")
+        rows = self.cur.fetchall()
+        return rows
+
+    def insert(self, pet, customer, retailer, price):
+        self.cur.execute("INSERT INTO parts VALUES (NULL, ?, ?, ?, ?)", (pet, customer, retailer, price))
+        self.conn.commit()
+
+    def remove(self, id):
+        self.cur.execute("DELETE FROM pet WHERE id=?", (id,))
+        self.conn.commit()
+
+    def update(self, id, pet, customer, retailer, price):
+        self.cur.execute("UPDATE pet SET pet = ?, customer = ?, retailer = ?, price = ? WHERE id = ?", (pet, customer, retailer, price, id))
+        self.conn.commit()
+
+    
+        
